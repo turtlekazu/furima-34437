3144448ffc4c777ff8e21ed7d3d5b5d844b34ed8
turtlekazu <turtlekazu@icloud.com>
2021-02-19 15:38:20 +0900

    Add basic_auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+  def basic_auth
+    authenticate_or_request_with_http_basic do |user_name, password|
+      user_name == ENV["FURIMA_BASIC_AUTH_USER"] && password == ENV["FURIMA_BASIC_AUTH_PASSWORD"]
+    end
+  end
 end
