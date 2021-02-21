11c16a1af4ed19837eaeb708486e49424d26028c
turtlekazu <turtlekazu@icloud.com>
2021-02-21 21:43:54 +0900

    Improve validation options code

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,15 +11,15 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :birth_date
-  end
 
-  with_options presence: true, format: { with: /\A[ぁ-ゔァ-ヴ一-龥]+\z/, message: 'Full-width characters' } do
-    validates :last_name
-    validates :first_name
-  end
+    with_options format: { with: /\A[ぁ-ゔァ-ヴ一-龥]+\z/, message: 'Full-width characters' } do
+      validates :last_name
+      validates :first_name
+    end
 
-  with_options presence: true, format: { with: /\A[ァ-ヴ]+\z/, message: 'Full-width katakana characters' } do
-    validates :last_name_kana
-    validates :first_name_kana
+    with_options format: { with: /\A[ァ-ヴ]+\z/, message: 'Full-width katakana characters' } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
   end
 end
