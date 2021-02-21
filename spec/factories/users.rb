e42a09dae766075e22070540565b11707be6fa81
turtlekazu <turtlekazu@icloud.com>
2021-02-21 18:57:12 +0900

    Add factorybot user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,16 @@
 FactoryBot.define do
   factory :user do
+    transient do
+      japanese_name {Gimei.name}
+    end
+    nickname              {Faker::Name.name}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Lorem.characters(min_alpha: 3, min_numeric: 3, number: 10)}
+    password_confirmation {password}
+    last_name             {japanese_name.last.kanji}
+    first_name            {japanese_name.first.kanji}
+    last_name_kana        {japanese_name.last.katakana}
+    first_name_kana       {japanese_name.first.katakana}
+    birth_date            {Faker::Date.backward}
   end
 end
