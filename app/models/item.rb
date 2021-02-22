dbc64d4e58058693a4be89d9e6dce998950b1c01
turtlekazu <turtlekazu@icloud.com>
2021-02-22 12:40:26 +0900

    Add  basic validations to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,22 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :info
+    validates :price
+
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :sales_status_id
+      validates :shipping_fee_id
+      validates :prefecture_id
+    end
+  end
+
   belongs_to :user
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category, :sales_status, :shipping_fee, :prefecture, :scheduled_delivery
+  belongs_to :category
+  belongs_to :sales_status
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
 end
