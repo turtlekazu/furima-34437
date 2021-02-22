70e317d81de63e8560bdd134816c83f67d7b3fef
turtlekazu <turtlekazu@icloud.com>
2021-02-22 20:20:42 +0900

    Add test image & factory_bot item model

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
+    name                   {Faker::Device.model_name}
+    info                   {Faker::Device.manufacturer}
+    category_id            {rand(2..11)}
+    sales_status_id        {rand(2..7)}
+    prefecture_id          {rand(2..48)}
+    scheduled_delivery_id  {rand(2..4)}
+    price                  {rand(300..9999999)}
     
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.jpg'), filename: 'img')
+    end
   end
 end
