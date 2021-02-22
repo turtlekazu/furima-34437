1e404b36409b3eb458b909706cc6c3714b917e77
turtlekazu <turtlekazu@icloud.com>
2021-02-22 22:20:34 +0900

    Change validation of price column

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ApplicationRecord
     validates :info
     validates :image
 
-    validates :price, format: { with: /\A[0-9]+\z/, message: 'Half-width number' }, inclusion: { in: 300..9999999, message: 'Out of setting range' }
+    validates :price, numericality: { message: 'Half-width number' }, inclusion: { in: 300..9999999, message: 'Out of setting range' }
 
     with_options numericality: { other_than: 1, message: 'Select' } do
       validates :category_id
