dcd385a38f6a1e10127d252067a52fcccbc810aa
turtlekazu <turtlekazu@icloud.com>
2021-02-22 12:31:28 +0900

    Add associations between items & activehashes

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :sales_status, :shipping_fee, :prefecture, :scheduled_delivery
 end
