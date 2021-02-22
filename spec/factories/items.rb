cd8171424642e5970238574fead727cfc1cb972d
turtlekazu <turtlekazu@icloud.com>
2021-02-22 22:20:16 +0900

    Add shipping_fee_id to factorybot

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -4,10 +4,11 @@ FactoryBot.define do
     info                   {Faker::Device.manufacturer}
     category_id            {rand(2..11)}
     sales_status_id        {rand(2..7)}
+    shipping_fee_id        {rand(2..3)}
     prefecture_id          {rand(2..48)}
     scheduled_delivery_id  {rand(2..4)}
     price                  {rand(300..9999999)}
-    
+
     association :user
 
     after(:build) do |item|
