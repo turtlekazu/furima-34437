ef81aabd422c9abb0f5a8917597fbf17570450f1
turtlekazu <turtlekazu@icloud.com>
2021-02-23 11:36:26 +0900

    Add regex to price validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ApplicationRecord
     validates :name
     validates :info
 
-    validates :price, numericality: { message: 'Half-width number' },
+    validates :price, numericality: { with: /\A[0-9]+\z/, message: 'Half-width number' },
                       inclusion: { in: 300..9_999_999, message: 'Out of setting range' }
 
     with_options numericality: { other_than: 1, message: 'Select' } do
