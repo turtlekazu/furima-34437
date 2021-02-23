6e9131ccd44bc19dc07ce65a10ef19d9c7a27744
turtlekazu <turtlekazu@icloud.com>
2021-02-23 10:21:27 +0900

    Improve calc_tax_profit if condition

--- app/javascript/calc_tax_profit.js
+++ app/javascript/calc_tax_profit.js
@@ -1,15 +1,17 @@
 function calc_tax_profit () {
-  if(document.getElementById('item-price') != null) {
-    const itemPrice = document.getElementById('item-price');
-    itemPrice.addEventListener('keyup', () => {
-      const tax = Math.floor(itemPrice.value / 10);
-      const profit = itemPrice.value - tax;
-      const taxArea = document.getElementById('add-tax-price');
-      const profitArea = document.getElementById('profit');
-      taxArea.innerHTML = tax;
-      profitArea.innerHTML = profit;
-    });
+  if(document.getElementById('item-price') == null) {
+    return;
   }
+
+  const itemPrice = document.getElementById('item-price');
+  itemPrice.addEventListener('keyup', () => {
+    const tax = Math.floor(itemPrice.value / 10);
+    const profit = itemPrice.value - tax;
+    const taxArea = document.getElementById('add-tax-price');
+    const profitArea = document.getElementById('profit');
+    taxArea.innerHTML = tax;
+    profitArea.innerHTML = profit;
+  });
 };
 
 window.addEventListener('load', calc_tax_profit);
