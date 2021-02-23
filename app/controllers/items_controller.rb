6d36d040c23fba477a5b189d33c888521730eb8e
turtlekazu <turtlekazu@icloud.com>
2021-02-23 19:18:33 +0900

    Change redirect_path when finish edit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,7 +26,7 @@ class ItemsController < ApplicationController
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
-      redirect_to root_path
+      redirect_to item_path(@item)
     else
       render action: :edit
     end
