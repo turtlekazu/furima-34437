9e0f5653182052a447f335dc8a1b1e8312886655
turtlekazu <turtlekazu@icloud.com>
2021-02-24 15:58:52 +0900

    Fix create_action to work correctly

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,20 +3,18 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @order = Item.find(params[:id]).order.new(order_params)
-    @shipping = @order.shipping.new(shipping_params)
-    if @order.save && @shipping.save
+    @order = Order.create(user_id: current_user.id, item_id: params[:item_id])
+    @shipping = Shipping.new(shipping_params.merge(order_id: @order.id))
+    binding.pry
+    if @shipping.save
       redirect_to root_path
     else
-      render action: :new
+      @order.destroy
+      render action: :index
     end
   end
 
   private
-
-  def order_params
-    params.merge(user_id: current_user.id)
-  end
 
   def shipping_params
     params.require(:shipping).permit(:postal_code, :prefecture_id, :city, :address_line1, :address_line2, :phone_number)
