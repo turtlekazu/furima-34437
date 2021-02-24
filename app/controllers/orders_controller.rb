d1e5701ec270b4ce505b3731447dc8bb56bbd7b7
turtlekazu <turtlekazu@icloud.com>
2021-02-24 14:58:27 +0900

    Add order_controller

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,24 @@
+class OrdersController < ApplicationController
+  def index
+  end
+
+  def create
+    @order = Item.find(params[:id]).order.new(order_params)
+    @shipping = @order.shipping.new(shipping_params)
+    if @order.save && @shipping.save
+      redirect_to root_path
+    else
+      render action: :new
+    end
+  end
+
+  private
+
+  def order_params
+    params.merge(user_id: current_user.id)
+  end
+
+  def shipping_params
+    params.require(:order).permit(:postal_code, :prefecture_id, :city, :address_line1, :address_line2, :phone_number)
+  end
+end
