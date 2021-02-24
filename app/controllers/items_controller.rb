f2e08762b5423484a24cfb4295a57c0a28412dcf
turtlekazu <turtlekazu@icloud.com>
2021-02-24 12:09:55 +0900

    Add destroy_action to find_item method

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :find_item, only: [:edit, :update, :show]
+  before_action :find_item, only: [:edit, :update, :show, :destroy]
   before_action :send_to_root, only: [:edit, :update, :destroy]
 
   def index
@@ -35,8 +35,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
+    @item.destroy
     redirect_to root_path
   end
 
