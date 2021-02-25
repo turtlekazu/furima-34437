14c9b81981679210eace35520beb036e34332239
turtlekazu <turtlekazu@icloud.com>
2021-02-25 15:43:03 +0900

    Add factorybot of order_shipping

---
+++ spec/factories/order_shippings.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :order_shipping do
+    postal_code {'123-4567'}
+    prefecture_id {rand(2..48)}
+    city {Faker::Address.city}
+    address_line1 {Faker::Address.secondary_address}
+    address_line2 {Faker::Address.building_number}
+    phone_number {'01234567890'}
+  end
+end
