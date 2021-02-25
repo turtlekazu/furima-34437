1a338424436984c0acb3280be5afdb78e24c9a57
turtlekazu <turtlekazu@icloud.com>
2021-02-25 19:36:54 +0900

    Add redirect_to_root unless permitted_user

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,12 @@
 class OrdersController < ApplicationController
+  before_action :find_item, only: [:index, :create]
+  before_action :send_to_root, only: [:index, :create]
+
   def index
-    @item = Item.find(params[:item_id])
     @order_shipping = OrderShipping.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_shipping = OrderShipping.new(order_params)
     if @order_shipping.valid?
       pay_item
@@ -29,5 +30,15 @@ class OrdersController < ApplicationController
       card: order_params[:token],
       currency: 'jpy'
     )
+  end
+
+  def find_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def send_to_root
+    if user_signed_in? == false || current_user.id == @item.user_id || @item.order != nil
+      redirect_to root_path
+    end
   end
 end
