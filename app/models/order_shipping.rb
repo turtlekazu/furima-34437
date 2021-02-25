dcfd2bb8f6af11380e5931ab9b9f38a44eb2be6d
turtlekazu <turtlekazu@icloud.com>
2021-02-25 19:06:58 +0900

    Add token validation

--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -1,8 +1,9 @@
 class OrderShipping
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address_line1, :address_line2, :phone_number
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address_line1, :address_line2, :phone_number, :token
 
   with_options presence: true do
+    validates :token
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input correctly"}
     validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
     validates :city
