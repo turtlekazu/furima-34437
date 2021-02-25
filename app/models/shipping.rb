9d17d2418327951997b4bd1a70652bad73393553
turtlekazu <turtlekazu@icloud.com>
2021-02-25 12:00:54 +0900

    Remove extra validations

--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -1,11 +1,3 @@
 class Shipping < ApplicationRecord
-  with_options presence: true do
-    validates :postal_code
-    validates :prefecture_id
-    validates :city
-    validates :address_line1
-    validates :phone_number
-  end
-
   belongs_to :order
 end
