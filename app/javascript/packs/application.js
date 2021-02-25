511261b0d068f21c0bad80669e1b51fd8db13ed1
turtlekazu <turtlekazu@icloud.com>
2021-02-25 19:08:02 +0900

    Add card.js & require in application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../calc_tax_profit")
+require("../card")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
